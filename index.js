b1ff9378f5d98bcae700056a4fef18f2ad05b82e
RamPrasathRP <ramprasathrp2003@gmail.com>
2025-04-07 19:17:49 +0530

    completed

---
+++ index.js
@@ -0,0 +1,31 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomDiceImage1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomDiceImage2);
+
+// players
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "⛳️ Player 1 Wins";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "⛳️ Player 2 Wins";
+} else {
+  document.querySelector("h1").innerHTML = "😎 Match TIE";
+}
+
+// reload function
+
+function reloadPage() {
+  location.reload();
+}
